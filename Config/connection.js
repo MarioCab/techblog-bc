287bd478fc6b7d1d6f2108ef05935f67b5afab05
Mario Cabrera <yankeescab@gmail.com>
2021-06-29 02:24:54 -0400

    changed db storage to remotemysql

--- Config/connection.js
+++ Config/connection.js
@@ -3,15 +3,14 @@ require("dotenv").config();
 
 let sequelize;
 
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: "remotemysql.com",
-      dialect: "mysql",
-      port: "3306",
-    }
-  );
-}
+sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "remotemysql.com",
+    dialect: "mysql",
+    port: "3306",
+  }
+);
 module.exports = sequelize;
